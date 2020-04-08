bf893a71a5b585a8923377aa961af4b90259d595
spiderxman <3130569@qq.com>
2020-04-09 00:22:43 +0900

    no message

--- src/main/java/hrms/model/Company.java
+++ src/main/java/hrms/model/Company.java
@@ -5,13 +5,29 @@ import af.base.orm.annotation.Table;
 import lombok.Data;
 
 @Data
-@Table(name = "T_COMPANY", schema = "${commonSchema}")
+@Table(name = "T_B_COMPANY", schema = "${hrmsdb01Schema}")
 public class Company extends BaseDataModel {
 
     private static final long serialVersionUID = 1L;
 
-    private String COMPANY_ID;
+    private String userId;
 
-    private String COMPANY_NAME;
+    private String userName;
+
+    private String userNameDisp;
+
+    private String tel;
+
+    private String mail;
+
+    private String password;
+
+    private String companyName;
+
+    private String companyURL;
+
+    private String establishYear;
+
+    private String staffNum;
 
 }
--- src/main/java/hrms/wapi/company/CompanyService.java
+++ src/main/java/hrms/wapi/company/CompanyService.java
@@ -1,23 +1,39 @@
-package hrms.wapi.login;
+package hrms.wapi.company;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import af.base.service.BaseService;
 import af.main.model.User;
 import hrms.model.Company;
 import hrms.model.Constants;
+import hrms.wapi.base.HrmsSimpleDaoService;
 
 /*************************************************************************
  * Copyright     Asatecx Co.,Ltd.<br/>
- * Package       pf.main.wapi.login<br/>
- * Class         LoginService<br/>
- * Summary       ログインサービス<br/>
+ * Package       pf.main.wapi.company<br/>
+ * Class         CompanyService<br/>
+ * Summary       会社サービス<br/>
  * <br/>
  * Date             Author      Category     Version     Note<br/>
- * 2020/03/12       tang-xf     Add          1.0.0       新規作成<br/>
+ * 2020/04/09       tang-xf     Add          1.0.0       新規作成<br/>
  ************************************************************************/
-@Service
-public class LoginService extends BaseService {
+@Service("hrms.companyService")
+public class CompanyService extends HrmsSimpleDaoService {
+
+	/**
+     * 会社情報登録.
+     * @param company
+     * @return boolean
+     */
+	@Transactional("hrms.tx")
+	public boolean registCompany(Company company) {
+		Company checkCompany = super.find(company);
+		if (checkCompany != null) {
+			return false;
+		}
+		super.insert(true, company);
+		return true;
+	}
     /**
      * Select user without password.
      * @param userId
@@ -26,7 +42,7 @@ public class LoginService extends BaseService {
     public User getUser(String userId) {
         User user = new User();
         user.setUSER_ID(userId);
-        user = mainDao.find(user);
+        user = super.find(user);
         if (user != null) {
             if (!Constants.VALID_FLG_TRUE.equals(user.getVALID_FLG())) {
                 user = null;
@@ -45,7 +61,7 @@ public class LoginService extends BaseService {
     public Company getCompany(String companyId) {
         Company company = new Company();
         company.setUserId(companyId);
-        company = mainDao.find(company);
+        company = super.find(company);
         return company;
     }
 
