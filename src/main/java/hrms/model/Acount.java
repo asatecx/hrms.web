1432f9bccb72a05aea817aa4a0ea221d2566eb66
niucaocao6 <souryuu4@gmail.com>
2020-04-25 15:38:07 +0900

    no message

---
+++ src/main/java/hrms/model/Acount.java
@@ -0,0 +1,83 @@
+package hrms.model;
+
+import java.io.Serializable;
+
+public class Acount implements Serializable{
+    //下面是表单绑定的data 数据
+   
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String furikana;
+	private String userId;
+    /**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+
+	private String password;
+    private String email;
+	public Acount(String name,String furikana,String password,String email) {
+		super();
+	   this.name=name;
+	   this.furikana=furikana;
+	   this.password=password;
+	   this.email=email;
+	}
+	public Acount() {
+		super();
+
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFurikana() {
+		return furikana;
+	}
+
+	public void setFurikana(String furikana) {
+		this.furikana = furikana;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+    
+    @Override
+	public String toString() {
+		return "Acount [name=" + name + ", furikana=" + furikana + ", password=" + password + ", email=" + email + "]";
+	}
+
+}
--- src/main/java/hrms/model/Skill.java
+++ src/main/java/hrms/model/Skill.java
@@ -1,13 +1,28 @@
 package hrms.model;
 
 public class Skill {
-
+	private String skillcategory;
 	private String skill;
 
 	private String period;
 
 	private String level;
-
+	/**
+	 * 
+	 */
+	public Skill() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param skillcategory
+	 * @param skill
+	 */
+	public Skill(String skillcategory, String skill) {
+		super();
+		this.skillcategory = skillcategory;
+		this.skill = skill;
+	}
 	public void setSkill(String skill) {
 		this.skill = skill;
 	}
