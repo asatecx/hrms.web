499c836fe67fda84d8fa4c6ba12ccf8255b2d94a
niucaocao6 <souryuu4@gmail.com>
2020-05-09 12:42:25 +0900

    菜单里进行了细分

---
+++ src/main/java/hrms/model/Phase.java
@@ -0,0 +1,17 @@
+package hrms.model;
+
+import lombok.Data;
+
+@Data
+public class Phase {
+
+	private String	phase_rd	;
+	private String	phase_bd	;
+	private String	phase_fd	;
+	private String	phase_dd	;
+	private String	phase_cd	;
+	private String	phase_ut	;
+	private String	phase_it	;
+	private String	phase_st	;
+	private String	phase_ot	;
+}
